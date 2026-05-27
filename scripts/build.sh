3409bd9db336c9bbd437c7832992ade33f79e70c
Douglas M. <douglasmassolari@hotmail.com>
2026-05-27 19:06:17 +0200

    fix(build): run `gleam build` in script

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,4 +4,6 @@ rm -rf ./dist
 
 GLEAM_VERSION=$(gleam --version | awk '{print $2}')
 
+gleam build
+
 bun build --compile --outfile=./dist/radio_cli ./build/dev/javascript/radio_cli/gleam@@private_main_v$GLEAM_VERSION.mjs
